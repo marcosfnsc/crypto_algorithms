5fa7775ddb9f7605db32ad18253ff058c51d5cf8
Marcos Fonseca <marco12.fonseca@hotmail.com>
2020-10-13 11:08:33 -0300

    funcao para criar tabela está pronta

--- vegenere.py
+++ vegenere.py
@@ -1,5 +1,23 @@
+import string
+
+def _criar_tabela():
+    alfabeto = list(string.ascii_lowercase)
+    tabela = []
+
+    for x in range(len(string.ascii_lowercase)):
+        linha = alfabeto[:]
+        tabela.append(linha)
+
+        alfabeto.append(alfabeto[0])
+        del(alfabeto[0])
+
+    return tabela
+
 def encriptar(mensagem:str, chave: str) -> str:
     pass
 
 def decriptar(mensagem:str, chave: str) -> str:
+    pass
+
+if __name__ == '__main__':
     pass
