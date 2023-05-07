92cfba862b968c686a780984c90b555d42939c58
Marcos Fonseca <marco12.fonseca@hotmail.com>
2023-05-07 17:18:56 -0300

    codigo da cifra de cesar refatorado

--- cifras/caesar_cipher.py
+++ cifras/caesar_cipher.py
@@ -1,42 +1,44 @@
-class CaesarCipher:
-    def __init__(self):
-        self.ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+import string
 
-    def cipher(self, message, dir, rot):
-        msg = ''
-        for char in message:
-            if char in self.ALPHABET:
-                char_index = self.ALPHABET.index(char)
-                msg += self.ALPHABET[(char_index + (dir * rot)) % len(self.ALPHABET)]
-            else:
-                msg += char
-        return msg
+def fix_index(idx: int) -> int:
+    '''
+    caso o indice esteja fora do intervalo de simbolos
+    do alfabeto, essa função incrementa ou decrementa
+    o indice para caber dentro do intervalo
+    '''
+    while idx > len(string.ascii_letters):
+        idx -= len(string.ascii_letters)
+    while idx < 0:
+        idx += len(string.ascii_letters)
+    return idx
 
-    def encrypt(self, message, rot):
-        return self.cipher(message, 1, rot)
+def cipher(message: str, rot: int):
+    message = list(message)
 
-    def decrypt(self, message, rot):
-        return self.cipher(message, -1, rot)
+    for (idx, character) in enumerate(message):
+        idx_character = string \
+                .ascii_letters \
+                .find(character)
+        message[idx] = string \
+                .ascii_letters[fix_index(idx_character + rot)]
 
-    def main(self):
-
-        print("Você deseja encriptar ou decriptar a mensagem ?")
-        print("1 - Encriptar\n2 - Decriptar")
-        command = int(input())
-
-        print("Digite a mensagem: ")
-        message = input()
-        
-        print("Quantas rotações ?")
-        rot = int(input())
-
-        if command == 1:
-            print (self.encrypt(message, rot))
-        elif command == 2:
-            print (self.decrypt(message, rot))
-        else:
-            print ("Opção inválida")
+    return ''.join(message)
 
 if __name__ == '__main__':
-    cipher = CaesarCipher()
-    cipher.main()
+    print('Você deseja encriptar ou decriptar a mensagem ?')
+    print('1 - Encriptar\n2 - Decriptar')
+    command = int(input())
+
+    print('Digite a mensagem: ', end='')
+    message = input().strip()
+
+    print('Quantas rotações?: ', end='')
+    rot = int(input())
+
+    match command:
+        case 1:
+            print(cipher(message, rot))
+        case 2:
+            print(cipher(message, rot * -1))
+        case _:
+            print('Opção inválida')
